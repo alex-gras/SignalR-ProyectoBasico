ef28eef7cf16dc266b32e4712ff147f69281fe19
Alex Gras <a.gras@avanade.com>
2024-12-06 13:47:26 +0100

    Add project files.

---
+++ ClienteSignalR/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using Microsoft.AspNetCore.SignalR.Client;
+
+var connection = new HubConnectionBuilder()
+    .WithUrl("https://localhost:7139/messages")
+    .WithAutomaticReconnect()
+    .Build();
+
+connection.On<string, string>("UpdateAllAsync",
+    (user,message) => {
+        Console.WriteLine($"Message received: {user} - {message}");
+    });
+
+await connection.StartAsync();
+Console.WriteLine("Connection started.");
+
+while (true)
+{
+    Console.Write("Exit: pulsa una tecla");
+    var message = Console.ReadLine();
+    //NO FUNCIONA EL ENVÍO
+    //await connection.InvokeAsync("SendMessage", "alex", message);
+    break;
+}
+
+public class Notification
+{
+    public string Message { get; set; }
+}
---
+++ SignalRApi/SignalRApi/Controllers/NotificationController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using SignalRApi.SignalR;
+
+
+namespace SignalRApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class NotificationController : Controller
+    {
+
+        private readonly IHubContext<ChatHub> _hubContext;
+
+        public NotificationController(IHubContext<ChatHub> chatHub)
+        {
+            _hubContext = chatHub;
+        }
+
+        [HttpPost("sendNotification")]
+        public async Task<IActionResult> SendNotification([FromBody] Notification notification)
+        {
+            await _hubContext.Clients.All.SendAsync("UpdateAllAsync", notification.User, notification.Message);
+            return Ok(new { Message = "Notification sent successfully" });
+        }
+    }
+    public class Notification
+    {
+        public string User { get; set; }
+        public string Message { get; set; }
+    }
+}
+
